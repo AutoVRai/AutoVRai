fe14b27d3c358f7b30d1859b81dd5fd8c9e1d685
Brian Jorden <12365487+brianjorden@users.noreply.github.com>
2023-06-09 16:22:10 -0400

    expanded based on new functions

--- autovrai/__init__.py
+++ autovrai/__init__.py
@@ -1,14 +1,26 @@
 # this is my hacky way to more easily expose only the limited things I wanted to expose
 # as 'autovrai' to be imported by other files
 
+from gui import launch_gui
 from config import load_schema, load_defaults, handle_argparse, interpret_config
 from model import (
     model_loader,
+    set_model_precision,
     ZoeDepth_DepthModel,
     ZoeDepth_colorize,
     ZoeDepth_save_raw_16bit,
 )
-from gui import launch_gui
-from utilities import prep_directories, find_filenames, load_image
+from utilities import (
+    prep_directories,
+    find_filenames,
+    load_image,
+    load_video,
+    determine_file_or_path,
+)
+from process import (
+    process_image_directory,
+    process_video_directory,
+    process_single_image,
+    process_single_video,
+)
 from stereo import stereo_eyes, combine_stereo, combine_padded, combine_anaglyph
-from process import process_image_directory
